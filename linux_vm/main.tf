3c150e6d1f3636b9f65545666f551086460977f8
Ammar Ahmed <ammarejaz1874@gmail.com>
2021-07-02 19:25:41 +0500

    Update main.tf

--- linux_vm/main.tf
+++ linux_vm/main.tf
@@ -2,22 +2,66 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_key_vault_secret" "resourcegroupname" {
+name         = "resourcegroupname"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "resourcegrouplocation" {
+name         = "resourcegrouplocation"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "virtualnetworkname" {
+name         = "virtualnetworkname"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "subnetname" {
+name         = "subnetname"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "publicipname" {
+name         = "publicipname"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "networksecuritygroupname" {
+name         = "networksecuritygroupname"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "networkinterfacename" {
+name         = "networkinterfacename"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+data "azurerm_key_vault_secret" "linuxvirtualmachinename" {
+name         = "linuxvirtualmachinename"
+key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+}
+
+
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-
+  name     = data.azurerm_key_vault_secret.resourcegroupname.value
+  location = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  
   tags = {
-    environment = "salman"
+    environment = "salman",
+    purpose = "testing"
+
   }
 }
 
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
+  name                = data.azurerm_key_vault_secret.virtualnetworkname.value
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  depends_on = [azurerm_resource_group.rg]
 
   tags = {
     environment = "salman"
@@ -26,19 +70,32 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = data.azurerm_key_vault_secret.subnetname.value
+  resource_group_name  = data.azurerm_key_vault_secret.resourcegroupname.value
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [azurerm_resource_group.rg]
+
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "public_ip" {
-  name                = var.public_ip_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.publicipname.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
   allocation_method   = "Dynamic"
+  depends_on = [azurerm_resource_group.rg]
+  tags = {
+    environment = "salman"
+  }
+}
 
+resource "azurerm_public_ip" "public_ipsec" {
+  name                = "public_ipsec"
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  allocation_method   = "Dynamic"
+  depends_on = [azurerm_resource_group.rg]
   tags = {
     environment = "salman"
   }
@@ -46,9 +103,10 @@ resource "azurerm_public_ip" "public_ip" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "nsg" {
-  name                = var.network_security_group_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.networksecuritygroupname.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  depends_on = [azurerm_resource_group.rg]
 
   security_rule {
     name                       = "SSH"
@@ -69,9 +127,10 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                = var.network_interface_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = data.azurerm_key_vault_secret.networkinterfacename.value
+  location            = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name = data.azurerm_key_vault_secret.resourcegroupname.value
+  depends_on = [azurerm_resource_group.rg]
 
   ip_configuration {
     name                          = "myNicConfiguration"
@@ -85,11 +144,14 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "association" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+
 
 # Generate random text for a unique storage account name
 #resource "random_id" "randomId" {
@@ -115,7 +177,6 @@ resource "azurerm_network_interface_security_group_association" "association" {
 #}
 
 # Create (and display) an SSH key
-
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -123,11 +184,12 @@ resource "tls_private_key" "example_ssh" {
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "linuxvm" {
-  name                  = var.linux_virtual_machine_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  name                  = data.azurerm_key_vault_secret.linuxvirtualmachinename.value
+  location              = data.azurerm_key_vault_secret.resourcegrouplocation.value
+  resource_group_name   = data.azurerm_key_vault_secret.resourcegroupname.value
   network_interface_ids = [azurerm_network_interface.nic.id]
   size                  = "Standard_DS1_v2"
+  depends_on = [azurerm_resource_group.rg]
 
   os_disk {
     name                 = "myOsDisk"
@@ -160,17 +222,40 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   #}
 }
 
+
+#create another resource
+#data "azurerm_key_vault_secret" "resourcegroup" {
+#name         = "resourcegroup"
+#key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+#}
+
+#data "azurerm_key_vault_secret" "resourcegrouplocations" {
+#name         = "resourcegrouplocations"
+#key_vault_id = "/subscriptions/4a5b5785-dec0-4465-a13b-794c650c26d1/resourceGroups/kopicloud-tstate-rg/providers/Microsoft.KeyVault/vaults/secretkeyvalut1"
+#}
+
+# Create a resource group if it doesn't exist
+#resource "azurerm_resource_group" "rgs" {
+  #name     = data.azurerm_key_vault_secret.resourcegroup.value
+  #location = data.azurerm_key_vault_secret.resourcegrouplocations.value
+  
+  #tags = {
+    #environment = "salman",
+    #purpose = "testing"
+
+  #}
+#}
+
+
 #create a terraform tf file
 terraform {
   backend "azurerm" {
     resource_group_name   = "kopicloud-tstate-rg"
-    storage_account_name  = "kopicloudtfstate18830"
+    storage_account_name  = "kopicloudtfstate2"
     container_name        = "tfstate2"
     key                   = "tf_state_file"
   }
 }
-
-#access_key: pAv41ebjoB70mAR4ZEUxkowueVXLf5WE67GKSv+6GPfz2YnUveu7DGjVlGfb/v/dJdj4HNA+v3rRhyJyS20H3g==
 
 
 
